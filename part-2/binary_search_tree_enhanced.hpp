1deaea06fc9270fdebf66bdd07c93345617b3261
r8ndeer <liboting.lance2227@gmail.com>
2025-03-12 07:34:01 +0000

    up to Q6.2, using while loop

--- part-2/binary_search_tree_enhanced.hpp
+++ part-2/binary_search_tree_enhanced.hpp
@@ -3,14 +3,24 @@
 
 #include "binary_search_tree.hpp"
 
-template <typename T> T bst_min(const T &tree)
+template <typename T>
+T bst_min(const T& tree)  // T: reference to a pointer "tree"
 {
     // WRITE YOUR CODE HERE
+    T tree_min = tree;
+    while (left(tree_min))
+        tree_min = left(tree_min);
+    return tree_min;
 }
 
-template <typename T> T bst_max(const T &tree)
+template <typename T> 
+T bst_max(const T& tree)
 {
     // WRITE YOUR CODE HERE
+    T tree_max = tree;
+    while (right(tree_max))
+        tree_max = right(tree_max);
+    return tree_max;
 }
 
 #endif // __binary_saerch_tree_enhanced__
--- part-2/binary_tree_enhanced.hpp
+++ part-2/binary_tree_enhanced.hpp
@@ -12,6 +12,7 @@ template <typename V> struct BinaryTreeEnhanced {
     std::unique_ptr<BinaryTreeEnhanced<V>> _right;
 
     // WRITE YOUR CODE HERE
+    BinaryTreeEnhanced<V>* _parent;  // raw pointer because node doesn't own parent
 
     friend V &value(BinaryTreeEnhanced *t) { return t->_value; }
     friend const V &value(const BinaryTreeEnhanced *t)
@@ -26,20 +27,34 @@ template <typename V> struct BinaryTreeEnhanced {
     {
         return t->_right.get();
     }
-    friend BinaryTreeEnhanced *parent(const BinaryTreeEnhanced *t)
+    friend BinaryTreeEnhanced* parent(const BinaryTreeEnhanced* t)
     {
         // WRITE YOUR CODE HERE
+        return t->_parent;    
     }
 };
 
 // A helper function to build an enhanced binary tree
 template <typename V>
 std::unique_ptr<BinaryTreeEnhanced<V>>
-make_binary_tree_enhanced(const V &value,
+make_binary_tree_enhanced(const V& value,
                           std::unique_ptr<BinaryTreeEnhanced<V>> l,
                           std::unique_ptr<BinaryTreeEnhanced<V>> r)
 {
     // WRITE YOUR CODE HERE
+    // std::unique_ptr<BinaryTreeEnhanced<V>> tree = 
+    //     std::make_unique<BinaryTreeEnhanced<V>>(
+    //         value, 
+    //         std::move(l), 
+    //         std::move(r));
+    // The above code requires a constructor that takes 3 arguments
+    auto tree = std::unique_ptr<BinaryTreeEnhanced<V>>{
+        new BinaryTreeEnhanced<V>{value, std::move(l), std::move(r)}};        
+    if (tree->_left)
+        tree->_left->_parent = tree.get();
+    if (tree->_right)
+        tree->_right->_parent = tree.get();
+    return tree;
 }
 
 #endif // __binary_tree_enhanced__
